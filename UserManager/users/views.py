9f9eee05a7a98b345c860759cc224ad161d04aa7
sajadgilga <majidstic@yahoo.com>
2021-04-16 21:26:00 +0430

    added get profile

--- UserManager/users/views.py
+++ UserManager/users/views.py
@@ -1,9 +1,15 @@
 from rest_framework.views import APIView
 
+from users.serializers import UserSerializer
+
 
 class ProfileView(APIView):
+    def serializer_class(self):
+        return UserSerializer
+
     def get(self, request):
-        pass
+        user = request.user
+        return self.serializer_class()(user).data
 
     def post(self, request):
         pass
