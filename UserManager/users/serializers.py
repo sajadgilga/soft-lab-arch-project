125dcc9ccac44f7b9321ce9d643de58a5c3cf9f9
sajadgilga <majidstic@yahoo.com>
2021-04-16 22:20:54 +0430

    added register, fixed some bugs in get profile

--- UserManager/users/serializers.py
+++ UserManager/users/serializers.py
@@ -4,6 +4,23 @@ from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    role = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'username', 'email', 'phone']
+        fields = ['id', 'first_name', 'password', 'last_name', 'username', 'email', 'phone', 'role']
+        read_only_fields = ['id', 'first_name', 'last_name', 'role']
+
+    def get_role(self, obj):
+        if obj.is_superuser:
+            return 'admin'
+        return obj.get_role_display()
+
+    def create(self, validated_data):
+        username, email, phone, password = validated_data['username'], validated_data['email'], validated_data['phone'], \
+                                           validated_data['password']
+        user = User.objects.create(username=username, email=email, phone=phone, role=User.Roles.CLIENT)
+        user.set_password(password)
+        user.save()
+        return user
--- UserManager/users/views.py
+++ UserManager/users/views.py
@@ -1,15 +1,42 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from users.serializers import UserSerializer
 
 
-class ProfileView(APIView):
-    def serializer_class(self):
+class RegisterView(APIView):
+    def get_serializer_class(self):
         return UserSerializer
+
+    def get_serializer(self, data):
+        return self.get_serializer_class()(data=data)
+
+    def serialize(self, instance):
+        return self.get_serializer_class()(instance).data
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response(self.serialize(user))
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        return UserSerializer
+
+    def get_serializer(self, instance, data):
+        return self.get_serializer_class()(instance, data=data, partial=True)
+
+    def serialize(self, instance):
+        return self.get_serializer_class()(instance).data
 
     def get(self, request):
         user = request.user
-        return self.serializer_class()(user).data
+        return Response(self.serialize(user))
 
-    def post(self, request):
+    def put(self, request):
         pass
