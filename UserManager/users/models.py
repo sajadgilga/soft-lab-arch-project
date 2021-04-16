6c398f107a636883e0830eedb67ef5cf5360da4e
sajadgilga <majidstic@yahoo.com>
2021-04-16 21:05:15 +0430

    initialized project added user base model

---
+++ UserManager/users/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Create your views here.
+
+phone_number_validator = RegexValidator(r'^[0-9]*$', 'Only phone_numbers are allowed.')
+
+
+class User(AbstractUser):
+    class Roles(models.IntegerChoices):
+        ADMIN = 0, _('admin')
+        CLIENT = 1, _('client')
+
+    phone = models.CharField(validators=phone_number_validator)
+    role = models.IntegerField(choices=Roles.choices, default=Roles.CLIENT)
