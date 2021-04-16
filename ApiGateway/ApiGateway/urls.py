e03bedd42deaaa72602ff888e127eac23f360843
sajadgilga <majidstic@yahoo.com>
2021-04-16 22:46:19 +0430

    added base gateway view

--- ApiGateway/ApiGateway/urls.py
+++ ApiGateway/ApiGateway/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from gateway.views import MainGatewayView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('*', MainGatewayView.as_view())
 ]
--- ApiGateway/gateway/views.py
+++ ApiGateway/gateway/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.views import View
+
+
+class MainGatewayView(View):
+    def redirect(self, request):
+        pass
+
+    def get(self, request):
+        return self.redirect(request)
+
+    def post(self, request):
+        return self.redirect(request)
+
+    def put(self, request):
+        return self.redirect(request)
